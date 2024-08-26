f1d234a47d4558f9c589e790b3d0de96221d252e
Musaab Javed <61820693+Crypto47@users.noreply.github.com>
2024-08-26 17:04:47 +0500

    Update script.js

--- widget/assets/js/script.js
+++ widget/assets/js/script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
 
     // Credentials
@@ -7,7 +6,7 @@ $(document).ready(function() {
     // You can also add the html content in html page and still it will work!
     var mybot = '<div class="chatCont" id="chatCont">'+
                                 '<div class="bot_profile">'+
-                                    '<img src="assets/img/bot2.svg" class="bot_p_img">'+
+                                    '<img src="https://crypto47.github.io/Widget/widget/assets/img/bot2.svg" class="bot_p_img">'+
                                     '<div class="close">'+
                                         '<i class="fa fa-times" aria-hidden="true"></i>'+
                                     '</div>'+
@@ -26,7 +25,7 @@ $(document).ready(function() {
                             '<div class="profile_div">'+
                                 '<div class="row">'+
                                     '<div class="col-hgt">'+
-                                        '<img src="assets/img/bot2.svg" class="img-circle img-profile">'+
+                                        '<img src="https://crypto47.github.io/Widget/widget/assets/img/bot2.svg" class="img-circle img-profile">'+
                                     '</div><!--col-hgt end-->'+
                                     '<div class="col-hgt">'+
                                         '<div class="chat-txt">'+
